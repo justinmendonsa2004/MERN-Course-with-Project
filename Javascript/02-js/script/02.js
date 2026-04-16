dc6bcc7bb168e734280c730d63b09ba6623763d3
justin <justinmendonsa2004@gmail.com>
2026-04-16 23:56:48 +0530

    update code

---
+++ Javascript/02-js/script/02.js
@@ -0,0 +1,28 @@
+console.log("subscribe to the channel");
+
+//comments
+console.log("Hello,world");
+
+/*variables*/
+
+var a = 10;
+var a;
+console.log(a);
+
+let b = 20;
+//let b; same variables can't declare
+console.log(b);
+
+const c = 40;
+//c = 60;
+console.log(c);
+
+d = 100;
+console.log(d);
+
+let num1 = 100;
+let Num1 = 200;
+
+/*1.varibale as case sensetive.
+  2.variable must start with name not number.
+  3.variable can start with underscore.*/
